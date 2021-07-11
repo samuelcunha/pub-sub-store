0713ee43006ecd81a015bb012b62a07d07109440
Samuel Cunha <samureira@gmail.com>
2021-07-11 18:09:41 -0300

    Gerando relatório de vendas por produto

--- services/report/app.js
+++ services/report/app.js
@@ -4,27 +4,36 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 var report = {}
-async function updateReport(products) {
-    for(let product in products) {
-        if(!product.name) {
-            continue
-        } else if(!report[product.name]) {
-            report[product.name] = 1;
-        } else {
-            report[product.name]++;
-        }
-    }
 
+async function processMessage(msg) {
+    const orderData = JSON.parse(msg.content);
+    await updateReport(orderData);
+    await printReport();
+}
+
+async function updateReport(orderData) {
+    if (orderData.products && orderData.products.length) {
+        orderData.products.forEach(product => {
+            if (product.name) {
+                if (!report[product.name]) {
+                    report[product.name] = 1;
+                } else {
+                    report[product.name]++;
+                }
+            }
+        });
+    }
 }
 
 async function printReport() {
     for (const [key, value] of Object.entries(report)) {
         console.log(`${key} = ${value} vendas`);
-      }
+    }
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
-} 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => { processMessage(msg) })
+}
 
 consume()
